8fbb42f139ab55d2fac4346555c776282bdac3dd
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-28 18:34:50 +0200

    app-third

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,63 @@
 <template>
-  <div id="app">App</div>
+  <div id="app">
+    <Head @startSearch="startSearch" />
+
+    <Main v-if="results.movie.length > 0" type="movie" :list="results.movie" />
+    <Main v-if="results.tv.length > 0" type="tv" :list="results.tv" />
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import Head from "./components/Head.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    Head,
+    Main,
+  },
 
   data() {
     return {
-      apiUrl: "https://api.themoviedb.org/3/search/movie",
+      apiUrl: "https://api.themoviedb.org/3/search/",
       apiKey: "514edf9961ca2d85a642a68d752ce4a9",
+      results: {
+        movie: [],
+        tv: [],
+      },
     };
   },
 
   methods: {
-    getAPI(query) {
+    startSearch(obj) {
+      this.resetResults();
+      if (obj.type === "all") {
+        this.getAPI(obj.text, "movie");
+        this.getAPI(obj.text, "tv");
+      } else {
+        this.getAPI(obj.text, obj.type);
+      }
+    },
+
+    resetResults() {
+      this.results.movie = [];
+      this.results.tv = [];
+    },
+
+    getAPI(query, type) {
       axios
-        .get(this.apiUrl, {
+        .get(this.apiUrl + type, {
           params: {
-            api_Key: this.apiKey,
+            api_key: this.apiKey,
             query: query,
             language: "it-IT",
           },
         })
         .then((res) => {
-          console.log(res.data);
+          this.results[type] = res.data.results;
+          console.log(this.results);
         })
         .catch((err) => {
           console.log(err);
@@ -35,9 +65,7 @@ export default {
     },
   },
 
-  created() {
-    this.getAPI("star wars");
-  },
+  created() {},
 };
 </script>
 

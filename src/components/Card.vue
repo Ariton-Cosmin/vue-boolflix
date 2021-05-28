c8d4dba5ceacefbecc9d2dafbd95d4814f8fae50
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-28 18:35:32 +0200

    card-first

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul>
+      <li><span>Title:</span> {{ card.title }}</li>
+      <li>
+        <span>Original name:</span>
+        {{ card.original_title || card.original_name }}
+      </li>
+      <li><span>Langauge:</span> {{ card.original_language }}</li>
+      <li><span>Vote:</span> {{ card.vote_average }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  width: 80%;
+  padding: 1rem;
+
+  ul {
+    border-radius: 10px;
+    overflow: hidden;
+    background-color: white;
+
+    li {
+      line-height: 30px;
+      padding-left: 5px;
+      list-style: none;
+      border: 1px solid rgba(0, 0, 0, 0.3);
+
+      span {
+        color: gray;
+      }
+    }
+  }
+}
+</style>

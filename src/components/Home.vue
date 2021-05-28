60c7f8f32e0caf6c04fa433ee6e843ec6b79ba3a
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-28 14:49:06 +0200

    home-second

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,34 +5,14 @@
 <script>
 export default {
   name: "Home",
-  props: {
-    home: Object,
-  },
 
-  data() {
+  d /* ata() {
     return {
       apiURL: "https://api.themoviedb.org/3/search/movie",
       apiKey: "514edf9961ca2d85a642a68d752ce4a9",
       query: "ritorno al futuro",
     };
-  },
-
-  created() {
-    axis
-      .get(this.apiURL, {
-        params: {
-          api_key: this.apiKey,
-          query: this.query,
-          language: "it-IT",
-        },
-      })
-      .then((resp) => {
-        console.log(resp.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
+  }, */,
 };
 </script>
 

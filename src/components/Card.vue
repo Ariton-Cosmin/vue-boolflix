cedf5955ae09b52579ca30ee7bb5aa0f06515947
Cosmin-Ariton <cosminiris12@gmail.com>
2021-06-01 10:25:11 +0200

    card-third

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,65 @@
 <template>
   <div>
-    <ul>
-      <li><span>Title:</span> {{ card.title || card.name }}</li>
-      <li>
-        <span>Original name:</span>
-        {{ card.original_title || card.original_name }}
-      </li>
+    <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <img
+            :src="imageURL + card.poster_path"
+            :alt="card.title || card.name"
+          />
+        </div>
 
-      <li v-if="flags.includes(card.original_language)">
-        <img
-          :src="require(`../assets/img/${card.original_language}.png`)"
-          :alt="card.original_language"
-        />
-      </li>
+        <div class="flip-card-back">
+          <p><strong>Titolo: </strong> {{ card.title || card.name }}</p>
 
-      <li v-else><span>Langauge:</span> {{ card.original_language }}</li>
-      <li><span>Langauge:</span> {{ card.original_language }}</li>
-      <li><span>Vote:</span> {{ card.vote_average }}</li>
-      <li class="overview"><span>overview:</span> {{ card.overview }}</li>
-    </ul>
+          <p>
+            <strong>Titolo originale: </strong>
+            {{ card.original_title || card.original_name }}
+          </p>
+
+          <p>
+            <strong>Lingua: </strong>
+            <img
+              class="flag"
+              v-if="flags.includes(card.original_language)"
+              :src="require(`../assets/img/${card.original_language}.png`)"
+              :alt="card.original_language"
+            />
+            <span v-else>{{ card.original_language }}</span>
+          </p>
+
+          <div>
+            <strong>Voto: </strong>
+            <div class="stars">
+              <div class="empty">
+                <div class="inner">
+                  <i class="far fa-star"></i>
+                  <i class="far fa-star"></i>
+                  <i class="far fa-star"></i>
+                  <i class="far fa-star"></i>
+                  <i class="far fa-star"></i>
+                </div>
+              </div>
+
+              <div class="solid" :style="`width:${6.7 * card.vote_average}px`">
+                <div class="inner">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <p class="overview">
+            <strong>Overview: </strong>
+            {{ card.overview }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,6 +73,7 @@ export default {
   data() {
     return {
       flags: ["it", "en"],
+      imageURL: "https://image.tmdb.org/t/p/w342",
     };
   },
 };
@@ -40,31 +82,80 @@ export default {
 <style lang="scss" scoped>
 div {
   width: 80%;
-  padding: 1rem;
+  margin-top: 1.5rem;
+  margin-bottom: 4.5rem;
 
-  ul {
-    border-radius: 10px;
+  .flip-card {
+    background-color: transparent;
+    width: 342px;
+    height: 513px;
+    perspective: 1000px;
+  }
+
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+
+  .flip-card-front,
+  .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    padding: 10px;
+    border: 1px solid white;
+    background-color: black;
     overflow: hidden;
-    background-color: white;
+  }
 
-    li {
-      line-height: 30px;
-      padding-left: 5px;
-      list-style: none;
-      border: 1px solid rgba(0, 0, 0, 0.3);
+  .flip-card-front {
+    color: black;
 
-      span {
-        color: gray;
-      }
-
-      img {
-        width: 40px;
-        padding-top: 9px;
-      }
+    img {
+      width: 100%;
     }
+  }
+
+  .flip-card-back {
+    line-height: 2;
+    color: white;
+    transform: rotateY(180deg);
+    overflow: scroll;
 
     .overview {
-      line-height: 20px;
+      line-height: 1.3;
+    }
+  }
+
+  .flag {
+    width: 30px;
+    vertical-align: middle;
+  }
+
+  .stars {
+    position: relative;
+    margin-bottom: -60px;
+
+    .empty,
+    .solid {
+      position: absolute;
+      top: -105px;
+      left: 50px;
+      overflow: hidden;
+    }
+
+    .inner {
+      width: 90px;
     }
   }
 }
